a6b9c2a93195671168064308ac31e0e2f95aa436
Debojyoti Ghosh <gdebojyoti@users.noreply.github.com>
2026-06-01 15:19:10 +0530

    Structure defined.

--- app/vsc/index.tsx
+++ app/vsc/index.tsx
@@ -1,6 +1,44 @@
 const VSC = () => {
   return (
-    <div>I got in</div>
+    <div>
+      {/* title bar - SKIP for now */}
+      
+      {/* container */}
+      <div>
+        {/* activity bar */}
+
+        {/* activity panel */}
+        <nav>
+          {/* title */}
+
+          {/* open editors list */}
+
+          {/* file / folder structure */}
+        </nav>
+
+        {/* main content */}
+        <main>
+          {/* editor panel */}
+          <div>
+            {/* editor panel header */}
+            <div></div>
+
+            {/* editor file container */}
+            <div>
+              {/* breadcrumb */}
+
+              {/* editor file contents */}
+            </div>
+          </div>
+
+          {/* terminal */}
+          <div></div>
+        </main>
+      </div>
+
+      {/* bottom bar - branch, error / warning, UTF, CRLF, language, etc*/}
+      <div></div>
+    </div>
   );
 };
 
