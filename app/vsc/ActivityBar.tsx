6ee7fdd2f2b8e2967a4140ea90041401fdba843f
Debojyoti Ghosh <gdebojyoti@users.noreply.github.com>
2026-08-04 06:40:28 +0530

    WIP.

--- app/vsc/ActivityBar.tsx
+++ app/vsc/ActivityBar.tsx
@@ -2,6 +2,10 @@ const ActivityBar = () => {
   return (
     <div className="w-12 bg-primary">
       {/* Activity Bar */}
+      <div className="w-full h-12 bg-card" />
+      <div className="w-full h-12 bg-card" />
+      <div className="w-full h-12 bg-card border-l-background border-l-2" />
+      <div className="w-full h-12 bg-card" />
     </div>
   )
 }
